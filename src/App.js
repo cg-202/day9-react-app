dd6a466f868ce96bd17ca4b7a4e82979ad9983c1
santosh <santosh.ece06@gmail.com>
2021-07-02 09:33:55 +0530

    react website.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
+      <MySpaTag />
+    </div>
+  );
+}
+
+function MySpaTag() {
+  const [list] = useState([1, 1, 1, 1, 1]);
+
+  return (
+    <div>
+      <h2 className="bg-dark text-light p-3 sticky-top">Project Book</h2>
+
+      {list.map((item, index) => (
+        <div key={index} className="bg-secondary text-light p-4 m-3 rounded">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+          necessitatibus temporibus quis fugit ad numquam cupiditate
+          perspiciatis natus magnam nemo ipsam ipsum facere eius, ipsa adipisci
+          aperiam reprehenderit vero impedit.
+        </div>
+      ))}
     </div>
   );
 }
