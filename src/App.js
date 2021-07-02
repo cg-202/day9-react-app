f81c9524868e2100cb7c5a97b966d800f60247c4
santosh <santosh.ece06@gmail.com>
2021-07-02 10:33:29 +0530

    react router.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from "react";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import { MyAppHomePage } from "./pages/MyAppHomePage";
+import { MyAppExplorePage } from "./pages/MyAppExplorePage";
+import { MyAppNotificationPage } from "./pages/MyAppNotificationPage";
+import { MyAppMessagesPage } from "./pages/MyAppMessagesPage";
+
 function App() {
   return (
     <Router>
@@ -30,82 +35,6 @@ function App() {
       <Route exact path="/notification" component={MyAppNotificationPage} />
       <Route exact path="/message" component={MyAppMessagesPage} />
     </Router>
-  );
-}
-
-function MyAppHomePage() {
-  const [list] = useState([1, 1, 1, 1, 1]);
-
-  return (
-    <div>
-      <h2 className="bg-dark text-light p-3 sticky-top">Home</h2>
-
-      {list.map((item, index) => (
-        <div key={index} className="bg-secondary text-light p-4 m-3 rounded">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          necessitatibus temporibus quis fugit ad numquam cupiditate
-          perspiciatis natus magnam nemo ipsam ipsum facere eius, ipsa adipisci
-          aperiam reprehenderit vero impedit.
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function MyAppExplorePage() {
-  const [list] = useState([1, 1, 1, 1, 1]);
-
-  return (
-    <div>
-      <h2 className="bg-dark text-light p-3 sticky-top">Explore</h2>
-
-      {list.map((item, index) => (
-        <div key={index} className="bg-info text-light p-4 m-3 rounded">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          necessitatibus temporibus quis fugit ad numquam cupiditate
-          perspiciatis natus magnam nemo ipsam ipsum facere eius, ipsa adipisci
-          aperiam reprehenderit vero impedit.
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function MyAppNotificationPage() {
-  const [list] = useState([1, 1, 1, 1, 1]);
-
-  return (
-    <div>
-      <h2 className="bg-dark text-light p-3 sticky-top">Notification</h2>
-
-      {list.map((item, index) => (
-        <div key={index} className="bg-success text-light p-4 m-3 rounded">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          necessitatibus temporibus quis fugit ad numquam cupiditate
-          perspiciatis natus magnam nemo ipsam ipsum facere eius, ipsa adipisci
-          aperiam reprehenderit vero impedit.
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function MyAppMessagesPage() {
-  const [list] = useState([1, 1, 1, 1, 1]);
-
-  return (
-    <div>
-      <h2 className="bg-dark text-light p-3 sticky-top">Messages</h2>
-
-      {list.map((item, index) => (
-        <div key={index} className="bg-warning text-light p-4 m-3 rounded">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-          necessitatibus temporibus quis fugit ad numquam cupiditate
-          perspiciatis natus magnam nemo ipsam ipsum facere eius, ipsa adipisci
-          aperiam reprehenderit vero impedit.
-        </div>
-      ))}
-    </div>
   );
 }
 
