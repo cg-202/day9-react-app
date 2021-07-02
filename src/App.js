981d55d14a80f80ca3c4265b50d2acfbf53898b0
santosh <santosh.ece06@gmail.com>
2021-07-02 12:22:11 +0530

    counter done.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { useState } from "react";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   return (
@@ -29,13 +29,36 @@ function App() {
 }
 
 function Page1() {
+  const dispatch = useDispatch();
   const state = useSelector((state) => state);
   // console.log("page1", state);
+
+  const incrementCounter = () => {
+    dispatch({ type: "COUNTER_INCREMENT" });
+  };
+
+  const decrementCounter = () => {
+    dispatch({ type: "COUNTER_DECREMENT" });
+  };
 
   return (
     <div>
       <h1>Page1 </h1>
       <div className="alert alert-secondary">Counter {state.counter}</div>
+
+      <input
+        type="button"
+        value="INCREMENT"
+        onClick={incrementCounter}
+        className="btn btn-secondary w-100 mb-1"
+      />
+
+      <input
+        type="button"
+        value="DECREMETN"
+        onClick={decrementCounter}
+        className="btn btn-primary w-100 mb-1"
+      />
 
       {state.cityList.map((item, index) => (
         <div key={index} className="alert alert-secondary">
--- src/redux/store.js
+++ src/redux/store.js
@@ -17,8 +17,16 @@ const initState = {
  * This functions helps us to UPDATE THE STATE; which declared as initial state.
  */
 function MyAppReducer(state = initState, action) {
-  // TODO UPDATE THE STATE
-  return state;
+  switch (action.type) {
+    case "COUNTER_INCREMENT":
+      let newcounter = state.counter + 100;
+      return { ...state, counter: newcounter };
+    case "COUNTER_DECREMENT":
+      let newcounter1 = state.counter - 10;
+      return { ...state, counter: newcounter1 };
+    default:
+      return state;
+  }
 }
 
 const store = createStore(MyAppReducer);
